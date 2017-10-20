cad5abc81326fa5075292b0225cce36c6259b729
Lefty <brjebrow@umail.iu.edu>
2017-10-20 07:43:57 -0400

    v1.2 Commit for Meeting

--- app/app.js
+++ app/app.js
@@ -1,3 +1,4 @@
+//animations that start at page runtime
 function runtimeAnimation() {
     $("html, body").fadeIn(700, function() {
         $('nav').stop().animate({
@@ -8,16 +9,65 @@ function runtimeAnimation() {
     });
 }
 
+//all click bindings
 function setBindings() {
+    //learn more click event
     $('#home-end-content, #home-end-content-downchevron').click(function() {
         $('html, body').animate({
            scrollTop: $('#about-section').offset().top
-        }, 1500, "easeInExpo");
+        }, 1250, "easeInExpo");
+    });
+
+    $('a:nth-child(1)').click(function() {
+        $('html, body').animate({
+            scrollTop: $('#home-section').offset().top
+        }, 1250, 'easeInExpo');
+    });
+
+    $('a:nth-child(2)').click(function() {
+        $('html, body').animate({
+            scrollTop: $('#about-section').offset().top
+        }, 1250, 'easeInExpo');
+    });
+
+    $('a:nth-child(3)').click(function() {
+        $('html, body').animate({
+            scrollTop: $('#team-section').offset().top
+        }, 1250, 'easeInExpo');
+    });
+
+    //initial navicon click event
+    //communicates down to closeNav() which then communicates with openNav() and so on and so on...
+    $('.fa-navicon').click(function() {
+        if ($('#nav-links').css('marginTop') === "-150px") {
+            $('#nav-links').stop().animate({
+                marginTop: "0"
+            }, 750, 'easeOutQuad');
+            $('.navicon').html("<i class='fa fa-close fa-3x close-click' aria-hidden='true' onclick='closeNav()'></i>");
+        }
     });
 }
 
+function openNav() {
+    if ($('#nav-links').css('marginTop') === "-150px") {
+        $('#nav-links').stop().animate({
+            marginTop: "0"
+        }, 750, 'easeOutQuad');
+        $('.navicon').html("<i class='fa fa-close fa-3x close-click' aria-hidden='true' onclick='closeNav()'></i>");
+    }
+}
+
+function closeNav() {
+    $('#nav-links').stop().animate({
+        marginTop: "-150px"
+    }, 400, 'easeInQuad');
+    $('.navicon').html("<i class='fa fa-navicon fa-3x' aria-hidden='true' onclick='openNav()'></i>");
+}
+
+
+//runs beginning animations and sets click bindings
 $(document).ready(function() {
     $("html, body, #home-end-content-holder, #slide-arrows").hide();
     runtimeAnimation();
-    setBindings();
+    $(setBindings())();
 });
